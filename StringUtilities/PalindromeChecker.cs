aa8e1a31e94245040e3a93ee31b69815b466663d
Mike Emo <mike.emo@healthland.com>
2015-04-06 08:29:01 -0500

    Initial Test

---
+++ StringUtilities/PalindromeChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringUtilities
+{
+    public class PalindromeChecker
+    {
+        public static bool IsPalindrome(string value)
+        {
+            string backwordValue = "";
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                backwordValue += value.Substring(i, 1);
+            }
+
+            return value == backwordValue;
+        }
+    }
+}
