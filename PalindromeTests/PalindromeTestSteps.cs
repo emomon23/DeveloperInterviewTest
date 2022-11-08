be1cfa871339adab85a592aaea3137152de90655
Patrick Graham <Patrick.Graham@drs.com>
2022-11-08 15:15:31 -0600

    Update PalindromeChecker
    * Check for null or empty strings and return false.
    * Compare characters one at a time to improve efficiency.
    * Convert characters to lowercase before comparing.
    * Add tests for long palindromes and short palindromes.
    * Add tests for strings with whitespace.
    * Add tests for strings with numbers.
    * Add tests for strings with special characters.
    * Add test for single character word.
    * Add documentation for methods and tests.
    * Remove unnecessary using statements.
    * Fix typo palindrom -> palindrome.

---
+++ PalindromeTests/PalindromeTestSteps.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringUtilities;
+
+namespace PalindromeTests
+{
+    /// <summary>
+    /// Helper class for testing IsPalindrome()
+    /// </summary>
+    public class PalindromeTestSteps
+    {
+        private string wordToCheck;
+        private bool isPalindrome;
+
+        /// <summary>
+        /// Sets the word to check.
+        /// </summary>
+        /// <param name="value">Sets the word to value.</param>
+        public void GivenAValueOf(string value)
+        {
+            wordToCheck = value;
+        }
+
+        /// <summary>
+        /// Calls IsPalindrome() on the stored word.
+        /// </summary>
+        public void WhenPalidromeCheckIsCalled()
+        {
+            isPalindrome = PalindromeChecker.IsPalindrome(wordToCheck);
+        }
+
+        /// <summary>
+        /// Asserts that the result of IsPalindrome() is true.
+        /// </summary>
+        public void ThenItShouldBeAPalindrome()
+        {
+            Assert.IsTrue(isPalindrome, wordToCheck + " was not regarded as a palindrome as was expected");
+        }
+
+        /// <summary>
+        /// Asserts that the result of IsPalindrome() is false.
+        /// </summary>
+        public void ThenItShouldNOTBeAPalindrome()
+        {
+            Assert.IsFalse(isPalindrome, wordToCheck + " was regarded as a palindrome, and it should not be");
+        }
+    }
+}
