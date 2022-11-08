3580391eac4311e2069f18b5a4647cd2c5d3c569
Tree-Eater <crothjoyce@gmail.com>
2022-11-07 20:34:17 -0600

    Make PalindromeChecker case-insensitive and robust against null values.
    Add more unit tests.
    
    Ignore .suo file that's generated by VS

--- PalindromeTests/Given_Palindrom_Logic_Tests.cs
+++ PalindromeTests/Given_Palindrom_Logic_Tests.cs
@@ -49,11 +49,75 @@ namespace PalindromeTests
         [TestMethod]
         public void When_Word_Is_SOME_REALLY_LONG_STRING_Not_A_Palindrome_Result_Should_Be_False()
         {
-            string someReallyLongString = null;
+            string someReallyLongString = "ThisIsSomeReallyLongStringThatNobodyWouldEverUse";
 
             steps.GivenAValueOf(someReallyLongString);
             steps.WhenPalidromeCheckIsCalled();
             steps.ThenItShouldNOTBeAPalindrom();
+        }
+
+        [TestMethod]
+        public void When_Word_Is_RaCeCaR_Result_Should_Be_True()
+        {
+            steps.GivenAValueOf("RaCeCaR");
+            steps.WhenPalidromeCheckIsCalled();
+            steps.ThenItShouldBeAPalindrome();
+        }
+
+        [TestMethod]
+        public void When_Word_Is_Random_Characters_Result_Should_Be_False()
+        {
+            steps.GivenAValueOf("$%&*#$&%");
+            steps.WhenPalidromeCheckIsCalled();
+            steps.ThenItShouldNOTBeAPalindrom();
+        }
+
+        [TestMethod]
+        public void When_Word_Is_Empty_String_Result_Should_Be_True()
+        {
+            steps.GivenAValueOf(string.Empty);
+            steps.WhenPalidromeCheckIsCalled();
+            steps.ThenItShouldBeAPalindrome();
+        }
+
+        [TestMethod]
+        public void When_Word_Is_Single_Character_Result_Should_Be_True()
+        {
+            steps.GivenAValueOf("R");
+            steps.WhenPalidromeCheckIsCalled();
+            steps.ThenItShouldBeAPalindrome();
+        }
+
+        [TestMethod]
+        public void When_Word_Is_Null_Result_Should_Be_False()
+        {
+            steps.GivenAValueOf(null);
+            steps.WhenPalidromeCheckIsCalled();
+            steps.ThenItShouldNOTBeAPalindrom();
+        }
+
+        [TestMethod]
+        public void When_Word_Is_Palindrome_With_Special_Characters_Result_Should_Be_True()
+        {
+            steps.GivenAValueOf("**PaaP**");
+            steps.WhenPalidromeCheckIsCalled();
+            steps.ThenItShouldBeAPalindrome();
+        }
+
+        [TestMethod]
+        public void When_Word_Is_Palindrome_In_Number_Form_Result_Should_Be_True()
+        {
+            steps.GivenAValueOf("1117777111");
+            steps.WhenPalidromeCheckIsCalled();
+            steps.ThenItShouldBeAPalindrome();
+        }
+
+        [TestMethod]
+        public void When_Word_Is_Palindrome_With_Whitespace_Result_Should_Be_True()
+        {
+            steps.GivenAValueOf("KooK KOOK kook");
+            steps.WhenPalidromeCheckIsCalled();
+            steps.ThenItShouldBeAPalindrome();
         }
     }
 }
